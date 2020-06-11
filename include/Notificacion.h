b62fcd795598a775af3909d94625201d5eeea060
Nicolas Reyes <nicoreyes64@gmail.com>
2020-06-11 20:45:15 -0300

    Agregar clase Notificacion

---
+++ include/Notificacion.h
@@ -0,0 +1,14 @@
+#ifndef NOTIFICACION_H
+#define NOTIFICACION_H
+
+#include "Mensaje.h"
+
+class Notificacion{
+    private:
+        Mensaje* mensaje;
+    public:
+        Notificacion(Mensaje* mensaje);
+        ~Notificacion();
+        Mensaje* getMensaje();
+        bool esDeAsignatura(Asignatura* a);
+}
---
+++ src/Notificacion.cpp
@@ -0,0 +1,15 @@
+#include "../include/Notificacion.h"
+
+Notificacion::Notificacion(Mensaje* mensaje){
+    this->mensaje = mensaje;
+}
+
+Notificacion::~Notificacion(){}
+
+Mensaje* Notificacion::getMensaje(){
+    return this->mensaje;
+}
+
+bool Notificacion::esDeAsignatura(){
+    //TODO
+}
