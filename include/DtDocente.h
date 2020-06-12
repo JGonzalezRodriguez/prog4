ddf3930f367ea7ace995968972e4aefb8281c1a3
Nicolas Reyes <nicoreyes64@gmail.com>
2020-06-12 05:01:42 -0300

    fix en declaracion de constructores DtDocente y DtEstudiante

--- include/DtDocente.h
+++ include/DtDocente.h
@@ -8,7 +8,7 @@ class DtDocente: public DtUsuario{
     private:
         instituto instituto;
     public:
-        DtDocente(instituto instituto);
+        DtDocente(instituto instituto, std::string nombre, std::string email, std::string imagen, std::string contrasenia);
         ~DtDocente();
         instituto getInstituto();
 };
--- include/DtEstudiante.h
+++ include/DtEstudiante.h
@@ -7,7 +7,7 @@ class DtEstudiante: public DtUsuario{
     private:
         std::string ci;
     public:
-        DtEstudiante(std::string ci);
+        DtEstudiante(std::string ci, std::string nombre, std::string email, std::string imagen, std::string contrasenia);
         ~DtEstudiante();
         std::string getCi();
 };
